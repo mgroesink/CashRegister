b79ffd91e96931ea1aa14d4a7f3e74d49d379fde
mgroesink <mroesink@rocvantwente.nl>
2018-02-08 17:44:01 +0100

    Terug naar oorspronkelijke opdracht

--- KassaAppTests/CashRegisterTests.cs
+++ KassaAppTests/CashRegisterTests.cs
@@ -14,29 +14,18 @@ namespace KassaApp.Tests
         [TestMethod()]
         public void CashbackTest()
         {
-            CashRegister k = new CashRegister(1000);
-            int b500, b200, b100, b50, b20, b10, b5;
-            int e2, e1;
-            int ec50, ec20, ec10, ec5, ec2, ec1;
+            CashRegister k = new CashRegister();
 
-                decimal wisselgeld = k.Cashback(9.98m, 10, out b500, out b200, out b100, out b50, out b20, out b10, out b5,
-                     out ec50, out ec20, out ec10, out ec5, out ec2, out ec1, out e2, out e1, true);
-
+            decimal wisselgeld = k.Cashback(9.98m, 10);
             Assert.AreEqual(wisselgeld, 0m);
 
-            decimal wisselgeld2 = k.Cashback(9.96m, 10, out b500, out b200, out b100, out b50, out b20, out b10, out b5,
-                out ec50, out ec20, out ec10, out ec5, out ec2, out ec1, out e2, out e1, true);
-
+            decimal wisselgeld2 = k.Cashback(9.96m, 10);
             Assert.AreEqual(wisselgeld2, 0.05m);
 
-            decimal wisselgeld3 = k.Cashback(9.92m, 10, out b500, out b200, out b100, out b50, out b20, out b10, out b5,
-                out ec50, out ec20, out ec10, out ec5, out ec2, out ec1, out e2, out e1, true);
-
+            decimal wisselgeld3 = k.Cashback(9.92m, 10);
             Assert.AreEqual(wisselgeld3, 0.1m);
 
-            decimal wisselgeld4 = k.Cashback(9.93m, 10, out b500, out b200, out b100, out b50, out b20, out b10, out b5,
-                out ec50, out ec20, out ec10, out ec5, out ec2, out ec1, out e2, out e1, true);
-
+            decimal wisselgeld4 = k.Cashback(9.93m, 10);
             Assert.AreEqual(wisselgeld4, 0.05m);
 
         }
@@ -46,13 +35,8 @@ namespace KassaApp.Tests
 
         public void NotEnoughPaidTest()
         {
-            CashRegister k = new CashRegister(1000);
-            int b500, b200, b100, b50, b20, b10, b5;
-            int e2, e1;
-            int ec50, ec20, ec10, ec5, ec2, ec1;
-
-            decimal wisselgeld = k.Cashback(9.98m, 5, out b500, out b200, out b100, out b50, out b20, out b10, out b5,
-                 out ec50, out ec20, out ec10, out ec5, out ec2, out ec1, out e2, out e1, true);
+            CashRegister k = new CashRegister();
+            decimal wisselgeld = k.Cashback(9.98m, 5);
         }
     }
 }
